0853bc608b124150e04d783b1cb9d91f1854fdd7
Sebastian <sebastian998.pl@gmail.com>
2025-04-15 19:19:37 +0200

    dodanie mozliwosci wyszukiwania ksiazek

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,44 @@ namespace Ksiegarnia
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Book> allBooks { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            allBooks = new ObservableCollection<Book>
+            {
+                new Book {title = "Wiedzmin"},
+                new Book {title = "Hobbit"},
+                new Book {title = "Gra o tron"}
+            };
+
+            BooksList.ItemsSource = new ObservableCollection<Book>();
         }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = SearchBox.Text.ToLower();
+
+            if (string.IsNullOrEmpty(searchText))
+            {
+                BooksList.ItemsSource = new ObservableCollection<Book>();
+                BooksList.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+              var filteredBooks = allBooks.Where(b => b.title.ToLower().Contains(searchText)).ToList();
+              BooksList.ItemsSource = new ObservableCollection<Book>(filteredBooks);
+
+              if (filteredBooks.Count > 0) 
+                BooksList.Visibility = Visibility.Visible;
+            }
+
+        }
+    }
+
+    public class Book {
+        public string title { get; set; }
+        
+
     }
 }
