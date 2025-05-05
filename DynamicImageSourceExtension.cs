c57fdb4dc7dfdef8bc72e4a5629ba1bab896523e
Sebastian <sebastian998.pl@gmail.com>
2025-05-05 15:23:39 +0200

    zmiany

--- DynamicImageSourceExtension.cs
+++ DynamicImageSourceExtension.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Media.Imaging;
 
@@ -17,18 +18,37 @@ namespace Ksiegarnia.MarkupExtensions
         {
             FileName = fileName;
         }
-       
+        // Konstruktor domyślny – potrzebny, aby XAML mógł utworzyć instancję i później ustawić właściwość FileName
+        public DynamicImageSourceExtension() { }
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            string path = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, "Images");
+            // Jeśli FileName nie jest ustawiony, zwracamy null lub placeholder
+            if (string.IsNullOrWhiteSpace(FileName))
+            {
+                return null;
+            }
+
+            string path = Path.Combine(
+    Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName,
+    "Images");
+            MessageBox.Show($"Ścieżka do obrazu: {path}"); // Debugowanie
+
             string fullPath = Path.Combine(path, FileName);
+
+            if (!File.Exists(fullPath))
+            {
+                return new BitmapImage(); // Lub zwróć obraz zastępczy
+            }
+
             var bitmap = new BitmapImage();
             bitmap.BeginInit();
             bitmap.UriSource = new Uri(fullPath, UriKind.Absolute);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
             bitmap.EndInit();
             return bitmap;
         }
 
-        
+
+
     }
 }
